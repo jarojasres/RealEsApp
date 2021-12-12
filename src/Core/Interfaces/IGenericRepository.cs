e2b93202a61f3f5ab31be755ecbd850d5de27d40
jarojasres <jarojasres@gmail.com>
2021-12-11 23:51:36 -0500

    Add Generic repository

---
+++ src/Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<IReadOnlyList<T>> ListAllAsync();
+
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification);
+
+        Task<int> CountAsync(ISpecification<T> specification);
+
+        Task<T> FirstAsync(ISpecification<T> spec);
+        Task<T> FirstOrDefaultAsync(ISpecification<T> spec);
+
+        Task<bool> AnyAsync(ISpecification<T> spec);
+
+        Task UpdateAsync(T entity);
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
+
+
+    }
+}
